0c7498587232a86616cc27a2ad4482a2969c557d
nmytsyk <nmytsyk@playtika.com>
2019-08-14 18:33:11 +0300

    correction according to comments

--- src/main/java/HomeWork_1/Task_1_1.java
+++ src/main/java/HomeWork_1/Task_1_1.java
@@ -7,29 +7,25 @@ public class Task_1_1 {
         double z = 321;
         double n = 346.512;
 
-        System.out.printf("Результат уравнения #1 (long): %d%n", longCalculation(x,y,z,n));
-        System.out.printf("Результат уравнения #2 (int): %d%n", intCalculation(x,y,z,n));
-        System.out.printf("Результат уравнения #3 (float): %f%n", floatCalculation(x,y,z,n));
-        System.out.printf("Результат уравнения #4 (double): %f", doubleCalculation(x,y,z,n));
+        System.out.printf("Результат уравнения #1 (long): %d%n", calculationEquationLong(x,y,z,n));
+        System.out.printf("Результат уравнения #2 (int): %d%n", calculationEquationLInt(x,y,z,n));
+        System.out.printf("Результат уравнения #3 (float): %f%n", calculationEquationFloat(x,y,z,n));
+        System.out.printf("Результат уравнения #4 (double): %f", calculationEquationDouble(x,y,z,n));
 
     }
-    static long longCalculation (double x, double y, double z, double n){
-        long result1 = (long) (x - (y + 123) / z * n);
-        return result1;
+    private static long calculationEquationLong (double x, double y, double z, double n){
+        return (long) (x - (y + 123) / z * n);
     }
 
-    static int intCalculation (double x, double y, double z, double n){
-        int result2 = (int) ((x + (50 - y * z) / n) - x);
-        return result2;
+    private static int calculationEquationLInt (double x, double y, double z, double n){
+        return (int) ((x + (50 - y * z) / n) - x);
     }
 
-    static float floatCalculation (double x, double y, double z, double n){
-        float result3 = (float) (x / (y * (z-1) - 564) + n);
-        return result3;
+    private static float calculationEquationFloat (double x, double y, double z, double n){
+        return (float) (x / (y * (z-1) - 564) + n);
     }
 
-    static double doubleCalculation (double x, double y, double z, double n){
-        double result4 = (-x / -y + z / (n+15));
-        return result4;
+    private static double calculationEquationDouble (double x, double y, double z, double n){
+        return (-x / -y + z / (n+15));
     }
 }
--- src/main/java/HomeWork_1/Task_1_2.java
+++ src/main/java/HomeWork_1/Task_1_2.java
@@ -4,12 +4,12 @@ public class Task_1_2 {
     public static void main(String[] args) {
 
             int x = 45685;
-            /*
+
             outputOneByOne(x);
         }
 
-        static void outputOneByOne(int x) {
-        //*/
+        private static void outputOneByOne(int x) {
+
             int a = x / 10000;
             int b = x / 1000 % 10;
             int c = x / 100 % 10;
